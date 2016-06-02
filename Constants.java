6b5b9ef9c8a42cfcaccaef858287595aff5f5caf
tnquang1416 <tnquang1416@users.noreply.github.com>
2016-06-02 22:20:40 +0700

    Add files via upload

---
+++ Constants.java
@@ -0,0 +1,8 @@
+package main.java.internationalization;
+
+public class Constants {
+	public static final String FRENCH = "French";
+	public static final String ENGLISH = "English";
+	public static final String VIETNAMESE = "Vietnamese";
+	public static final String BUNDLE_DIR = "main.resources.languages";
+}
